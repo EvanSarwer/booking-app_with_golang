a795f2f61988012c952e207fefc38f94c1a8f2bd
EvanSarwer <evansarwer1@gmail.com>
2025-02-10 23:45:34 +0600

    Data Types in Go

--- main.go
+++ main.go
@@ -3,11 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName =  "Go Conference"
-	const conferenceTickets = 50
-	var remainingTickets = 50
-	
-	fmt.Println("Welcome to",conferenceName, "booking application")
+	//var conferenceName = "Go Conference"          // variable declaration with initialize value is ok without specifying the type
+	// conferenceName := "Go Conference"            // syntactic sugar in Go 
+	var conferenceName string = "Go Conference"
+	const conferenceTickets int = 50
+	var remainingTickets uint = 50                  //unsigned integer can't be negative
+
+	fmt.Printf("nonferenceName is %T and conferenceTickets is %T\n", conferenceName, conferenceTickets)
+
+	fmt.Println("Welcome to", conferenceName, "booking application")
 	fmt.Printf("We have total of %v tickets and %v tickets are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets hete to attend")
+
+	var userName string
+	var userTickets int
+
 }
