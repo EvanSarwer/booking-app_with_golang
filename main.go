262a37c6c2be7261b1e65aaa20dfc07312b1a443
EvanSarwer <evansarwer1@gmail.com>
2025-02-11 23:57:49 +0600

    Loops in Go

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	//var conferenceName = "Go Conference"          // variable declaration with initialize value is ok without specifying the type
@@ -13,7 +16,7 @@ func main() {
 	//var bookings [50]string                                   // declare an array of 50 elements without assigning values
 	//define slice
 	//var bookings = []string{"Evan", "Sarwer", "Arif"}           // declare a slice of 3 elements and assign values
-	var bookings []string								   // declare a slice of 50 elements without assigning values
+	var bookings []string // declare a slice of 50 elements without assigning values
 
 	fmt.Printf("nonferenceName is %T and conferenceTickets is %T\n", conferenceName, conferenceTickets)
 
@@ -21,36 +24,35 @@ func main() {
 	fmt.Printf("We have total of %v tickets and %v tickets are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets hete to attend")
 
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
 
-	
+		fmt.Println("Please enter your first name:")
+		fmt.Scan(&firstName)
+		fmt.Println("Please enter your last name:")
+		fmt.Scan(&lastName)
+		fmt.Println("Please enter your email:")
+		fmt.Scan(&email)
+		fmt.Println("Please enter the number of tickets you want to book:")
+		fmt.Scan(&userTickets)
 
+		remainingTickets = remainingTickets - userTickets
 
+		//bookings[0] = firstName + " " + lastName          // assign value to the first element of the array
+		bookings = append(bookings, firstName+" "+lastName) // append value to the slice
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
-	//ask user for their name
-	//fmt.Println(&userName)        // pointer to the memory location of the variable
-	fmt.Println("Please enter your first name:")
-	fmt.Scan(&firstName)
-	fmt.Println("Please enter your last name:")
-	fmt.Scan(&lastName)
-	fmt.Println("Please enter your email:")
-	fmt.Scan(&email)
-	fmt.Println("Please enter the number of tickets you want to book:")
-	fmt.Scan(&userTickets)
+		fmt.Printf("Thak you %v %v, for booking %v tickets. You will receive a confirmation email at %v.\n", firstName, lastName, userTickets, email)
+		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 
-	remainingTickets = remainingTickets - userTickets
-
-	//bookings[0] = firstName + " " + lastName          // assign value to the first element of the array
-	bookings = append(bookings, firstName + " " + lastName)     // append value to the slice
-
-	
-
-	fmt.Printf("Thak you %v %v, for booking %v tickets. You will receive a confirmation email at %v.\n", firstName, lastName, userTickets, email)
-	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
-
-	fmt.Printf("There are all our bookings: %v", bookings)
+		firstNames := []string{}
+		for _, booking := range bookings {
+			var names = strings.Fields(booking)
+			firstNames = append(firstNames, names[0])
+		}
+		fmt.Printf("The first names of bookings are: %v\n", firstNames)
+	}
 
 }
