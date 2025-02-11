6f860bedb36f4703bfedc14707481ac7d4da26a0
EvanSarwer <evansarwer1@gmail.com>
2025-02-11 23:07:45 +0600

    arrays && slices

--- main.go
+++ main.go
@@ -8,6 +8,12 @@ func main() {
 	var conferenceName string = "Go Conference"
 	const conferenceTickets int = 50
 	var remainingTickets uint = 50 //unsigned integer can't be negative
+	//define array
+	//var bookings = [50]string{"Evan", "Sarwer", "Arif"}           // declare an array of 50 elements and assign values
+	//var bookings [50]string                                   // declare an array of 50 elements without assigning values
+	//define slice
+	//var bookings = []string{"Evan", "Sarwer", "Arif"}           // declare a slice of 3 elements and assign values
+	var bookings []string								   // declare a slice of 50 elements without assigning values
 
 	fmt.Printf("nonferenceName is %T and conferenceTickets is %T\n", conferenceName, conferenceTickets)
 
@@ -15,10 +21,15 @@ func main() {
 	fmt.Printf("We have total of %v tickets and %v tickets are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets hete to attend")
 
+
+	
+
+
+
 	var firstName string
 	var lastName string
 	var email string
-	var userTickets int
+	var userTickets uint
 	//ask user for their name
 	//fmt.Println(&userName)        // pointer to the memory location of the variable
 	fmt.Println("Please enter your first name:")
@@ -30,6 +41,16 @@ func main() {
 	fmt.Println("Please enter the number of tickets you want to book:")
 	fmt.Scan(&userTickets)
 
+	remainingTickets = remainingTickets - userTickets
+
+	//bookings[0] = firstName + " " + lastName          // assign value to the first element of the array
+	bookings = append(bookings, firstName + " " + lastName)     // append value to the slice
+
+	
+
 	fmt.Printf("Thak you %v %v, for booking %v tickets. You will receive a confirmation email at %v.\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+	fmt.Printf("There are all our bookings: %v", bookings)
 
 }
